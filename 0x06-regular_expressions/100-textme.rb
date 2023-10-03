782e100a43f37f424b603c869275da17fa7b4747
VictorMaona <maonavictor@gmail.com>
2023-10-03 12:48:09 -0700

    updated

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,24 @@
 #!/usr/bin/env ruby
-# Show sender receiver and flags
 
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(","
+# Verify that script contains one argument.
+if ARGV.length != 1
+  puts "Usage: #{$0} <log_entry>"
+  exit 1
+end
+
+# From the command line argument retrieve log entry.
+log_entry = ARGV[0]
+
+# Locate all instances that match, then combine them using ","
+matches = log_entry.scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/)
+
+# Verify if any matches were discovered.
+if matches.any?
+  # Join the matches with ","
+  result = matches.map { |match| match.join(",") }
+
+  # Print results
+  puts result.join("\n")
+else
+  puts "No matches found in the log entry."
+end
