5d3cb9e0e8ea0a74f873d10d08f04c8e0b20ea5d
VictorMaona <maonavictor@gmail.com>
2023-11-24 10:28:07 -0800

    updated

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,10 +1,6 @@
-# Using pkill, puppet manifest to terminate the process 'killmenow'
+# kill puppet manifest to terminate the process killmenow
 
-# To end the process declare an executable resource.
 exec { 'pkill':
-  # Indicate the command to be carried out.
   command  => 'pkill killmenow',
-  
-  # Utilize the shell as the command execution provider.
   provider => 'shell',
 }
