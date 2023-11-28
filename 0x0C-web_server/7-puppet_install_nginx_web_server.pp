3a235c620a81b22813b2650ba7f2500c9825ac73
VictorMaona <maonavictor@gmail.com>
2023-11-28 15:44:48 -0800

    updated

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,79 @@
+# add the nginx stable version
+exec { 'add nginx stable repo':
+  command => 'sudo add-apt-repository ppa:nginx/stable',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+# update the list of software packages
+exec { 'update packages':
+  command => 'apt-get update',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+# Set up nginx
+package { 'nginx':
+  ensure     => 'installed',
+}
+
+# Permit HTTP
+exec { 'allow HTTP':
+  command => "ufw allow 'Nginx HTTP'",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  onlyif  => '! dpkg -l nginx | egrep \'îi.*nginx\' > /dev/null 2>&1',
+}
+
+# alter the rights of a folder
+exec { 'chmod www folder':
+  command => 'chmod -R 755 /var/www',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+# produce an index file
+file { '/var/www/html/index.html':
+  content => "Hello World!\n",
+}
+
+# make an index file
+file { '/var/www/html/404.html':
+  content => "Ceci n'est pas une page\n",
+}
+
+# Add an error page and redirection.
+file { 'Nginx default config file':
+  ensure  => file,
+  path    => '/etc/nginx/sites-enabled/default',
+  content =>
+"server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+               root /var/www/html;
+        # If you are using PHP add index.php to the list.
+        index index.html index.htm index.nginx-debian.html;
+        server_name _;
+        location / {
+                # First attempt to serve request as file, then
+                # as directory, then fall back to displaying a 404.
+                try_files \$uri \$uri/ =404;
+        }
+        error_page 404 /404.html;
+        location  /404.html {
+            internal;
+        }
+        
+        if (\$request_filename ~ redirect_me){
+            rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+        }
+}
+",
+}
+# relaunch nginx
+exec { 'restart service':
+  command => 'service nginx restart',
+  path    => '/usr/bin:/usr/sbin:/bin',
+}
+
+# launch the nginx service
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
---
+++ 0x0C-web_server/README.md
@@ -0,0 +1,6 @@
+## 0x0C. Web server
+
+I started using a web server and learned how they operate through this project. 
+ALX gave me access to a personal server. I gained knowledge about how to move 
+files to my server using Fabric and SCP. I also finished configuring the 
+server using Nginx on a basic level.
