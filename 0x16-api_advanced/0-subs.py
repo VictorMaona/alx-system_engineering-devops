1e5131a07d239dc387966d9b333660905a279902
VictorMaona <maonavictor@gmail.com>
2024-01-16 05:37:49 -0800

    updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
-""" subs gets the amount of subscribers for a specific subreddit
 """
-from requests import get
+subs gets the amount of subscribers for specific subreddit
+"""
+
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """ Queries Reddit API and returns number of subscribers (not active users)
-    for a given subreddit.
-
-    Args:
-        subreddit (str): subreddit to query
-
-    Return:
-        number of current subscribers to `subreddit`, or 0 if `subreddit` is
-    invalid
-    """
-    response = get('https://www.reddit.com/r/{}/about.json'.format(subreddit),
-                   headers={'User-Agent': 'allelomorph-app0'})
-    # non-existent subreddits sometimes return 404
-    if response.status_code != 200:
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
         return 0
-    # and sometimes return a dummy JSON dict with only 'Listing' key
-    return response.json().get('data').get('subscribers', 0)
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
